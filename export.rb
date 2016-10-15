e69a1ecb6ab567b67d8ad8021d112adb7ba66ab3
Jimmy Börjesson <alcesleo@gmail.com>
2016-10-15 13:26:23 +0200

    Write export script

---
+++ export.rb
@@ -0,0 +1,42 @@
+require "http"
+require "json"
+require "fileutils"
+require "dotenv"
+
+Dotenv.load
+
+EXPORT_PATH      = File.expand_path("~/Dropbox/apps/wallet/exports")
+SUBFOLDER        = Date.today.iso8601
+FULL_EXPORT_PATH = File.join(EXPORT_PATH, SUBFOLDER)
+
+FileUtils.mkdir_p(File.join(EXPORT_PATH, SUBFOLDER))
+
+WALLET_AUTH = {
+  x_token: ENV.fetch("WALLET_API_KEY"),
+  x_user:  ENV.fetch("WALLET_EMAIL"),
+}
+
+
+def conn
+  @_CONN ||= HTTP.persistent("https://api.budgetbakers.com")
+                 .headers(WALLET_AUTH)
+                 .headers(content_type: "application/json")
+end
+
+def get_json(path)
+  JSON.parse(conn.get(path))
+end
+
+def write_to_file(path, data)
+  File.write(path, JSON.pretty_generate(data))
+end
+
+def export(api_path, name)
+  destination = File.join(FULL_EXPORT_PATH, name)
+  write_to_file(destination, get_json(api_path))
+end
+
+export("/api/v1/records", "records.json")
+export("/api/v1/categories", "categories.json")
+export("/api/v1/currencies", "currencies.json")
+export("/api/v1/accounts", "accounts.json")
